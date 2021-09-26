be905f68950e1bc3e7d9734971cc2111928270a5
Hotiakov <Fauxlivegame@gmail.com>
2021-09-26 19:20:09 +0300

    add currency

--- my-app/src/Compenents/ListItem.js
+++ my-app/src/Compenents/ListItem.js
@@ -34,7 +34,7 @@ export const ListItem = ({ itemList }) => (
         {itemList.map(item=>(
             <Item key={item.id} img={item.img}>
                 <p>{item.name}</p>
-                <p>{item.price}</p>
+                <p>{item.price.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'})}</p>
             </Item>
         ))}
     </List>
